427774d0d8e3b92de8ebf7729a1de179854ccf85
Mac Topp <macm.topp@gmail.com>
2019-09-08 14:35:30 -0600

    initial deploy

---
+++ send_mail.py
@@ -0,0 +1,20 @@
+import smtplib
+from email.mime.text import MIMEText
+
+def send_mail(customer, dealer, rating, comments):
+    port = 2525
+    smtp_server = 'smtp.mailtrap.io'
+    login = '432d8358fa27b8'
+    password = '7b67f9c71a12a1'
+    message = f"<h3>New Feedback Submission</h3><ul><li>Customer: {customer}</li><li>Dealer: {dealer}</li><li>Rating: {rating}</li><li>Comments: {comments}</li></ul>"
+
+    sender_email = 'email@example.com'
+    reciever_email = 'email2@example.com'
+    msg = MIMEText(message, 'html')
+    msg['Subject'] = 'Lexus Feedback'
+    msg['From'] = sender_email
+    msg['To'] = reciever_email
+
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.login(login, password)
+        server.sendmail(sender_email, reciever_email, msg.as_string())
